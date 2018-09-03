bb475161f7e47365a24d91abb31015fc43bb377f
DELLXPS12\amitb <amitbansal@gmail.com>
2018-09-03 12:50:02 -0400

    raspiSpringDemo added a demo project

---
+++ src/main/java/com/raspi/spring/demo/raspiLedDemo/RaspiLedDemoApplication.java
@@ -0,0 +1,15 @@
+package com.raspi.spring.demo.raspiLedDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaspiLedDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaspiLedDemoApplication.class, args);
+	}
+	
+	
+	
+}
---
+++ src/test/java/com/raspi/spring/demo/raspiLedDemo/RaspiLedDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.raspi.spring.demo.raspiLedDemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RaspiLedDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
